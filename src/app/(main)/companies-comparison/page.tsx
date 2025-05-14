0ca831148433d8c7559d77f93528ec88d5cc784f
BillyBooglyBob <123722533+BillyBooglyBob@users.noreply.github.com>
2025-05-14 22:44:27 +1000

    Added Suspense for the useSearchParams()

--- src/app/(main)/companies-comparison/page.tsx
+++ src/app/(main)/companies-comparison/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import SentimentChartPage from "@/components/dashboard/SentimentChartPage";
+import { Suspense } from "react";
 
 const CompanyComparisonPage = () => {
   return (
-    <SentimentChartPage
-      heading="Companies Sentiment Comparison"
-      backLink="/"
-      showCompanySelector={true}
-    />
+    <Suspense fallback={<div>Loading...</div>}>
+      <SentimentChartPage
+        heading="Companies Sentiment Comparison"
+        backLink="/"
+        showCompanySelector={true}
+      />
+    </Suspense>
   );
 };
 
