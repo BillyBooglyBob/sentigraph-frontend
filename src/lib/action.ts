0683e380b3fffdaaacfa2bc9cdd7ca9453103e38
BillyBooglyBob <123722533+BillyBooglyBob@users.noreply.github.com>
2025-05-15 22:47:09 +1000

    Changed permission setting

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -20,21 +20,21 @@ export async function handleLoginAddCookies(
 
   cookieStore.set("session_userid", userId, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: false,
     maxAge: 60 * 60 * 24 * 7, // One week
     path: "/",
   });
 
   cookieStore.set("session_access_token", accessToken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: false,
     maxAge: 60 * 60, // 60 minutes
     path: "/",
   });
 
   cookieStore.set("session_refresh_token", refreshToken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: false,
     maxAge: 60 * 60 * 24 * 7,
     path: "/",
   });
